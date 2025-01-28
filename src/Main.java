42e7b6411bc49b67deefe252e35e2a74380fb235
karurazer <karurazer@gmail.com>
2025-01-28 04:57:55 +0100

    add constructor, getters, setters

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 
 public class Main {
     public static void main(String[] args) {
-
+        Board board = new Board();
+        board.setPiece(new Pawn(1,1,1));
+        board.show_board();
     }
 }
--- src/Piece.java
+++ src/Piece.java
@@ -1,2 +1,39 @@
 public class Piece {
+    public static final int BLACK = 0;
+    public static final int WHITE = 1;
+    private final int color;
+    private int locationX;
+    private int locationY;
+    private boolean isAlive=true;
+
+    public Piece(int locationX, int locationY, int color) {
+        this.locationX = locationX;
+        this.locationY = locationY;
+        this.color = color;
+    }
+
+    public int getLocationX() {
+        return locationX;
+    }
+    public void setLocationX(int locationX) {
+        this.locationX = locationX;
+    }
+
+    public int getLocationY() {
+        return locationY;
+    }
+    public void setLocationY(int locationY) {
+        this.locationY = locationY;
+    }
+    public int getColor() {
+        return color;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setDead() {
+        isAlive = false;
+    }
 }
