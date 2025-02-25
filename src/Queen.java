79a852b828b9cdc1af719003e81aef60da07d7de
karurazer <karurazer@gmail.com>
2025-02-25 20:00:09 +0100

    Queen logic is done.

--- src/Queen.java
+++ src/Queen.java
@@ -8,9 +8,39 @@ public class Queen extends Piece {
     }
 
     @Override
-    public boolean canMove(String toChessLocation, Piece[][] board){
-        return true;
+    public boolean canMove(String toChessLocation, Piece[][] board) {
+        int[] from = chessLocationToXY(this.getChessLocation());
+        int[] to = chessLocationToXY(toChessLocation);
 
+        int fromX = from[0], fromY = from[1];
+        int toX = to[0], toY = to[1];
+
+        boolean isStraightMove = (fromX == toX || fromY == toY);
+        boolean isDiagonalMove = (Math.abs(fromX - toX) == Math.abs(fromY - toY));
+
+        if (!(isStraightMove || isDiagonalMove)) {
+            return false;
+        }
+
+        int stepX = Integer.compare(toX, fromX);
+        int stepY = Integer.compare(toY, fromY);
+        int x = fromX + stepX;
+        int y = fromY + stepY;
+
+        while (x != toX || y != toY) {
+            if (board[y][x] != null) {
+                return false;
+            }
+            x += stepX;
+            y += stepY;
+        }
+
+        if (board[toY][toX] != null && board[toY][toX].getColor().equals(this.getColor())) {
+            return false;
+        }
+
+        return true;
     }
+
 }
 
