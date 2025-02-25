bec03a389dfcf2e1ba249aa7248e66075901dfe9
karurazer <karurazer@gmail.com>
2025-02-25 19:39:03 +0100

    King logic is done.

--- src/King.java
+++ src/King.java
@@ -7,8 +7,22 @@ public class King extends Piece {
         super(chessLocation, color);
     }
     @Override
-    public boolean canMove(String toChessLocation, Piece[][] board){
-        return true;
+    public boolean canMove(String toChessLocation, Piece[][] board) {
+        int[] from = chessLocationToXY(this.getChessLocation());
+        int[] to = chessLocationToXY(toChessLocation);
 
+        int fromX = from[0], fromY = from[1];
+        int toX = to[0], toY = to[1];
+
+        if (Math.abs(toX - fromX) > 1 || Math.abs(toY - fromY) > 1) {
+            return false;
+        }
+
+        if (board[toY][toX] != null && board[toY][toX].getColor().equals(this.getColor())) {
+            return false;
+        }
+
+        return true;
     }
+
 }
