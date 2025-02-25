b3abe6c5f3abac43ee49cea52dc54d5525e875b1
karurazer <karurazer@gmail.com>
2025-02-25 19:42:03 +0100

    Knight logic is done.

--- src/Knight.java
+++ src/Knight.java
@@ -8,8 +8,30 @@ public class Knight extends Piece {
     }
 
     @Override
-    public boolean canMove(String toChessLocation, Piece[][] board){
-        return true;
+    public boolean canMove(String toChessLocation, Piece[][] board) {
+        int[] from = chessLocationToXY(this.getChessLocation());
+        int[] to = chessLocationToXY(toChessLocation);
 
+        int fromX = from[0], fromY = from[1];
+        int toX = to[0], toY = to[1];
+
+        int[][] possibleMoves = {
+                {fromX + 2, fromY + 1}, {fromX + 2, fromY - 1},
+                {fromX - 2, fromY + 1}, {fromX - 2, fromY - 1},
+                {fromX + 1, fromY + 2}, {fromX + 1, fromY - 2},
+                {fromX - 1, fromY + 2}, {fromX - 1, fromY - 2}
+        };
+
+        boolean validMove = false;
+        for (int[] move : possibleMoves) {
+            if (move[0] == toX && move[1] == toY) {
+                validMove = true;
+                break;
+            }
+        }
+        if (!validMove) return false;
+
+        return board[toY][toX] == null || !board[toY][toX].getColor().equals(this.getColor());
     }
+
 }
